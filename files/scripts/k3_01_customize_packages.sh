3a3021fc4a4ad4e9b7489437dfe040b22b280ab0
Register <458892+aieu@users.noreply.github.com>
2020-11-03 11:07:20 +0800

    Compact configuration
    - add automount for r2s, x86_64, k3 and miwifi-mini
    - add autosamba for r2s, x86_64, k3 and miwifi-mini
    - add xlnetacc for r2s, x86_64, k3, k2p and miwifi-mini
    - add default-settings for r2s, x86_64, k3, k2p and miwifi-mini
    - add openappfilter for r2s, x86_64 and k3
    - add serverchan for k2p and miwifi-mini
    - add passwall for k2p
    - add libopenssl-devcrypto for r2s, x86_64
    - disable packaging toochain and imagebuilder for all devices
    - k2p: remove passwall
    
    Cryptographic Hardware Accelerators:
    https://openwrt.org/docs/techref/hardware/cryptographic.hardware.accelerators
    
    Signed-off-by: Register <458892+aieu@users.noreply.github.com>

--- files/scripts/k3_01_customize_packages.sh
+++ files/scripts/k3_01_customize_packages.sh
@@ -11,9 +11,8 @@ sed -i 's|$(BRCMFMAC_4366C0)|$(IEEE8021X) kmod-brcmfmac k3wifi k3screenctrl|g' t
 # k3 proprietary wifi driver
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/k3-brcmfmac4366c-firmware package/k3/k3wifi
 # k3 screen
-git clone https://github.com/lwz322/luci-app-k3screenctrl package/k3/luci-app-k3screenctrl
-git clone https://github.com/lwz322/k3screenctrl_build package/k3/k3screenctrl_build
-
+git clone -b master --depth 1 --single-branch https://github.com/lwz322/luci-app-k3screenctrl package/k3/luci-app-k3screenctrl
+git clone -b master --depth 1 --single-branch https://github.com/lwz322/k3screenctrl_build package/k3/k3screenctrl_build
 
 # access control
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-accesscontrol package/new/luci-app-accesscontrol
@@ -21,6 +20,14 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-accessco
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome package/new/luci-app-adguardhome
 # AutoCore
 svn co https://github.com/project-openwrt/openwrt/branches/master/package/lean/autocore package/new/autocore
+# automount
+rm -rf ./feeds/packages/kernel/exfat-nofuse
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/automount package/new/automount
+svn co https://github.com/openwrt/packages/trunk/utils/antfs-mount package/utils/antfs-mount
+svn co https://github.com/openwrt/packages/trunk/kernel/antfs package/kernel/antfs
+svn co https://github.com/openwrt/openwrt/trunk/package/kernel/exfat package/kernel/exfat
+# autosamba
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/autosamba package/new/autosamba
 # arpbind
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind package/new/luci-app-arpbind
 # cpufreq
@@ -35,13 +42,13 @@ svn co https://github.com/openwrt/luci/branches/openwrt-18.06/applications/luci-
 # Filetransfer
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-filetransfer package/new/luci-app-filetransfer
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-lib-fs package/new/luci-lib-fs
-# frp, don't build golang binary files
+# frp
 rm -f ./feeds/luci/applications/luci-app-frps
 rm -f ./feeds/luci/applications/luci-app-frpc
 rm -rf ./feeds/packages/net/frp
 rm -f ./package/feeds/packages/frp
-git clone -b master --single-branch https://github.com/kuoruan/luci-app-frpc.git
-git clone -b master --single-branch https://github.com/lwz322/luci-app-frps.git
+git clone -b master --depth 1 --single-branch https://github.com/kuoruan/luci-app-frpc.git package/new/luci-app-frpc
+git clone -b master --depth 1 --single-branch https://github.com/lwz322/luci-app-frps.git package/new/luci-app-frps
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/frp package/new/frp
 # FullCone
 svn co https://github.com/Lienol/openwrt/trunk/package/network/fullconenat package/network/fullconenat
@@ -58,11 +65,11 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ipsec-vp
 # NetData
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-netdata package/new/luci-app-netdata
 # OpenClash
-git clone -b master --single-branch https://github.com/vernesong/OpenClash package/new/luci-app-openclash
+git clone -b master --depth 1 --single-branch https://github.com/vernesong/OpenClash package/new/luci-app-openclash
 rm -rf feeds/packages/libs/libcap
 svn co https://github.com/openwrt/packages/trunk/libs/libcap feeds/packages/libs/libcap
 # OpenAppFilter
-# git clone -b master --single-branch https://github.com/destan19/OpenAppFilter package/new/OpenAppFilter
+git clone -b master --depth 1 --single-branch https://github.com/destan19/OpenAppFilter.git package/new/OpenAppFilter
 # PassWall
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/new/luci-app-passwall
 rm -rf ./feeds/packages/net/kcptun
@@ -84,22 +91,24 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs packa
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray package/new/v2ray
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin package/new/v2ray-plugin
 # SeverChan
-git clone -b master --single-branch https://github.com/tty228/luci-app-serverchan package/new/luci-app-serverchan
+git clone -b master --depth 1 --single-branch https://github.com/tty228/luci-app-serverchan package/new/luci-app-serverchan
 # Scheduled Reboot
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot package/new/luci-app-autoreboot
 # Traffic Usage Monitor
-git clone -b master --single-branch https://github.com/brvphoenix/wrtbwmon package/new/wrtbwmon
-git clone -b master --single-branch https://github.com/brvphoenix/luci-app-wrtbwmon package/new/luci-app-wrtbwmon
+git clone -b master --depth 1 --single-branch https://github.com/brvphoenix/wrtbwmon package/new/wrtbwmon
+git clone -b master --depth 1 --single-branch https://github.com/brvphoenix/luci-app-wrtbwmon package/new/luci-app-wrtbwmon
 # Unblock Netease Music
-git clone -b master --single-branch https://github.com/cnsilvan/luci-app-unblockneteasemusic.git package/new/luci-app-unblockneteasemusic-go
+git clone -b master --depth 1 --single-branch https://github.com/cnsilvan/luci-app-unblockneteasemusic.git package/new/luci-app-unblockneteasemusic-go
+# UPNP
+rm -rf ./feeds/packages/net/miniupnpd
+svn co https://github.com/coolsnowwolf/packages/trunk/net/miniupnpd feeds/packages/net/miniupnpd
 # USB Printer
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-usb-printer package/new/luci-app-usb-printer
 # vlmcsd
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/vlmcsd package/new/vlmcsd
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vlmcsd package/new/luci-app-vlmcsd
-# UPNP
-rm -rf ./feeds/packages/net/miniupnpd
-svn co https://github.com/coolsnowwolf/packages/trunk/net/miniupnpd feeds/packages/net/miniupnpd
+# xlnetacc
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-xlnetacc package/new/luci-app-xlnetacc
 # Zerotier
 svn co https://github.com/project-openwrt/openwrt/branches/master/package/lean/luci-app-zerotier package/new/luci-app-zerotier
 # zram-swap
@@ -107,6 +116,6 @@ rm -rf package/system/zram-swap
 svn co https://github.com/openwrt/openwrt/trunk/package/system/zram-swap package/system/zram-swap
 
 # default settings and translate
-svn co https://github.com/vgist/OpenWrt-Packages/trunk/default-settings package/new/lean-translate
+cp -rf ../default-settings package/new/lean-translate
 
 exit 0

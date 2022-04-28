275c46485a7cde1d76305c3a462105ce3d4d60e3
Anonymous <458892+aieu@users.noreply.github.com>
2022-04-28 17:22:08 +0800

    svn instead of git
    Signed-off-by: Anonymous <458892+aieu@users.noreply.github.com>

--- scripts/k3_customization.sh
+++ scripts/k3_customization.sh
@@ -8,10 +8,10 @@ sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES 
 # k3 device packages
 sed -i 's|$(BRCMFMAC_4366C0)|$(IEEE8021X) kmod-brcmfmac k3wifi k3screenctrl|g' target/linux/bcm53xx/image/Makefile
 # k3 proprietary wifi driver
-svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/k3-brcmfmac4366c-firmware package/k3/k3wifi
+svn export -q https://github.com/coolsnowwolf/lede/trunk/package/lean/k3-brcmfmac4366c-firmware package/k3/k3wifi
 # k3 screen
-git clone -b master --depth 1 --single-branch https://github.com/lwz322/luci-app-k3screenctrl package/k3/luci-app-k3screenctrl
-git clone -b master --depth 1 --single-branch https://github.com/lwz322/k3screenctrl_build package/k3/k3screenctrl_build
+svn export -q https://github.com/lwz322/luci-app-k3screenctrl/trunk package/k3/luci-app-k3screenctrl
+svn export -q https://github.com/lwz322/k3screenctrl_build/trunk package/k3/k3screenctrl_build
 
 # mbedtls
 cp -f ../patches/201-Camellia-block-cipher.patch package/libs/mbedtls/patches/

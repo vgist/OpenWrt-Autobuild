c87b17192d3a0117fd92b96ff0e2c3e42a0e5975
Anonymous <458892+aieu@users.noreply.github.com>
2022-04-28 16:03:20 +0800

    update 00_init.sh
    Signed-off-by: Anonymous <458892+aieu@users.noreply.github.com>

--- scripts/00_prepare.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-git clone -b master --depth 1 --single-branch https://github.com/immortalwrt/immortalwrt
-git clone -b master --depth 1 --single-branch https://github.com/immortalwrt/packages immortalwrt-packages
-git clone -b master --depth 1 --single-branch https://github.com/immortalwrt/luci immortalwrt-luci
-
-exit 0

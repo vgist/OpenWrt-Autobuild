b41f5e67380b6a2d49b87bbb08de6d7b0f4f3d18
iqbal fasya <79214502+scoout@users.noreply.github.com>
2022-08-16 00:07:18 +0700

    Update x86_customization.sh

--- scripts/x86_customization.sh
+++ scripts/x86_customization.sh
@@ -17,10 +17,6 @@ CONFIG_TIFM_CORE=y
 CONFIG_TIFM_7XX1=y
 ' >> ./target/linux/x86/generic/config-5.10
 
-#passwall
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' >> feeds.conf.default
-
 source ./01_customize_packages.sh
 
 exit 0

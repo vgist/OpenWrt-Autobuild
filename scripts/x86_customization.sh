8138c196a287738591387ecd48fb959a16a4f079
iqbal fasya <79214502+scoout@users.noreply.github.com>
2022-08-15 23:48:38 +0700

    Update x86_customization.sh

--- scripts/x86_customization.sh
+++ scripts/x86_customization.sh
@@ -16,9 +16,10 @@ CONFIG_MMC_TIFM_SD=y
 CONFIG_TIFM_CORE=y
 CONFIG_TIFM_7XX1=y
 ' >> ./target/linux/x86/generic/config-5.10
+
 #passwall
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' >> feeds.conf.default
-echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' >> feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
 source ./01_customize_packages.sh
 

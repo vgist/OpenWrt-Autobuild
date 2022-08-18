d06e8755dbbd46035dbde1f1cd5f9efd441e8f40
iqbal fasya <79214502+scoout@users.noreply.github.com>
2022-08-19 06:43:27 +0700

    Update 01_customize_packages.sh

--- scripts/01_customize_packages.sh
+++ scripts/01_customize_packages.sh
@@ -27,12 +27,12 @@ done
 cp -rf ../immortalwrt-luci/applications/luci-app-cpufreq package/new/
 
 # dnsmasq: add filter aaa option
-#cp -rf ../patches/910-add-filter-aaaa-option-support.patch package/network/services/dnsmasq/patches/
-#patch -p1 -i ../patches/dnsmasq-add-filter-aaaa-option.patch
-#patch -d feeds/luci -p1 -i ../../../patches/filter-aaaa-luci.patch
+cp -rf ../patches/910-add-filter-aaaa-option-support.patch package/network/services/dnsmasq/patches/
+patch -p1 -i ../patches/dnsmasq-add-filter-aaaa-option.patch
+patch -d feeds/luci -p1 -i ../../../patches/filter-aaaa-luci.patch
 
 # dnsmasq: use nft ruleset for dns_redirect
-#patch -p1 -i ../patches/dnsmasq-use-nft-ruleset-for-dns_redirect.patch
+patch -p1 -i ../patches/dnsmasq-use-nft-ruleset-for-dns_redirect.patch
 
 # Filetransfer
 cp -rf ../immortalwrt-luci/applications/luci-app-filetransfer package/new/
@@ -40,27 +40,27 @@ cp -rf ../immortalwrt-luci/libs/luci-lib-fs package/new/
 
 # FullCone nat for nftables
 # patch kernel
-#cp -f ../immortalwrt/target/linux/generic/hack-5.10/952-net-conntrack-events-support-multiple-registrant.patch target/linux/generic/hack-5.10/
+cp -f ../immortalwrt/target/linux/generic/hack-5.10/952-net-conntrack-events-support-multiple-registrant.patch target/linux/generic/hack-5.10/
 # fullconenat-nft
-#cp -rf ../immortalwrt/package/network/utils/fullconenat-nft package/network/utils/
+cp -rf ../immortalwrt/package/network/utils/fullconenat-nft package/network/utils/
 # patch libnftnl
-#cp -rf ../immortalwrt/package/libs/libnftnl/patches package/libs/libnftnl/
-#sed -i '/PKG_INSTALL:=1/i\PKG_FIXUP:=autoreconf' package/libs/libnftnl/Makefile
-#rm -rf package/libs/libnftnl
-#cp -rf ../immortalwrt/package/libs/libnftnl package/libs/
+cp -rf ../immortalwrt/package/libs/libnftnl/patches package/libs/libnftnl/
+sed -i '/PKG_INSTALL:=1/i\PKG_FIXUP:=autoreconf' package/libs/libnftnl/Makefile
+rm -rf package/libs/libnftnl
+cp -rf ../immortalwrt/package/libs/libnftnl package/libs/
 # patch nftables
-#cp -f ../immortalwrt/package/network/utils/nftables/patches/002-nftables-add-fullcone-expression-support.patch package/network/utils/nftables/patches/
-#rm -rf package/network/utils/nftables/
-#cp -rf ../immortalwrt/package/network/utils/nftables package/network/utils/
+cp -f ../immortalwrt/package/network/utils/nftables/patches/002-nftables-add-fullcone-expression-support.patch package/network/utils/nftables/patches/
+rm -rf package/network/utils/nftables/
+cp -rf ../immortalwrt/package/network/utils/nftables package/network/utils/
 # patch firewall4
-#cp -rf ../immortalwrt/package/network/config/firewall4/patches package/network/config/firewall4/
-#sed -i 's|+kmod-nft-nat +kmod-nft-nat6|+kmod-nft-nat +kmod-nft-nat6 +kmod-nft-fullcone|g' package/network/config/firewall4/Makefile
+cp -rf ../immortalwrt/package/network/config/firewall4/patches package/network/config/firewall4/
+sed -i 's|+kmod-nft-nat +kmod-nft-nat6|+kmod-nft-nat +kmod-nft-nat6 +kmod-nft-fullcone|g' package/network/config/firewall4/Makefile
 # patch luci
-#patch -d feeds/luci -p1 -i ../../../patches/fullconenat-luci.patch
+patch -d feeds/luci -p1 -i ../../../patches/fullconenat-luci.patch
 
 # mbedtls
-#cp -f ../immortalwrt/package/libs/mbedtls/patches/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch package/libs/mbedtls/patches/
-#cp -f ../patches/201-Camellia-block-cipher.patch package/libs/mbedtls/patches/
+cp -f ../immortalwrt/package/libs/mbedtls/patches/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch package/libs/mbedtls/patches/
+cp -f ../patches/201-Camellia-block-cipher.patch package/libs/mbedtls/patches/
 
 # OpenClash
 svn export -q https://github.com/kenzok8/openwrt-packages/trunk/luci-app-openclash package/new/luci-app-openclash
@@ -92,8 +92,8 @@ svn export -q https://github.com/koshev-msk/xmm-modem/trunk package/xmm-modem
 sed -i 's,16384,65536,g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
 # need ImmortalWrt mirrors
-#cp -f ../immortalwrt/scripts/download.pl scripts/
-#cp -f ../immortalwrt/include/download.mk include/
+cp -f ../immortalwrt/scripts/download.pl scripts/
+cp -f ../immortalwrt/include/download.mk include/
 
 # fix include luci.mk
 find package/new/ -type f -name Makefile -exec sed -i 's,../../luci.mk,$(TOPDIR)/feeds/luci/luci.mk,g' {} +

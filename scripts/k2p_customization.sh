ae9c277272da10495c40bd9e715164a5cd598951
Anonymous <458892+aieu@users.noreply.github.com>
2022-09-08 13:02:55 +0800

    mt76: vht on 2.4 patch

--- scripts/k2p_customization.sh
+++ scripts/k2p_customization.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+# Allow chips that support 11ac to use 256QAM on 2.4GHz
+cp -rf ../immortalwrt/package/kernel/mt76/patches package/kernel/mt76/
+
 source ./01_customize_packages.sh
 
 exit 0

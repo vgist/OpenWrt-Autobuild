c39c271f5e79839ee2f6b72d70c297bd0d3920b4
Anonymous <458892+aieu@users.noreply.github.com>
2022-09-13 15:45:01 +0800

    chore: refine k3 DEVICE_PACKAGES
    (cherry picked from commit c14c65b8ed4d3238e7baaa1c8a41de29a6520485)

--- scripts/k3_customization.sh
+++ scripts/k3_customization.sh
@@ -6,7 +6,7 @@ set -ex
 # only build k3
 sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
 # k3 device packages
-sed -i 's|kmod-brcmfmac brcmfmac-firmware-4366c0-pcie|kmod-brcmfmac k3wifi k3screenctrl|g' target/linux/bcm53xx/image/Makefile
+sed -i '/K3/{N;s/.*/  DEVICE_MODEL := K3\n  DEVICE_PACKAGES := $(BRCMFMAC_4366C0) $(USB3_PACKAGES) -brcmfmac-firmware-4366c0-pcie k3wifi k3screenctrl/}' target/linux/bcm53xx/image/Makefile
 # k3 proprietary wifi driver
 cp -rf ../lede/package/lean/k3-brcmfmac4366c-firmware package/firmware/
 # k3 screen control

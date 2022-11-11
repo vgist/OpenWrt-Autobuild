446a50e4c71d71c15e02a9cc3fc22c966108ba65
iqbal fasya <79214502+scoout@users.noreply.github.com>
2022-11-11 08:27:23 +0700

    Update x86_customization.sh

--- scripts/x86_customization.sh
+++ scripts/x86_customization.sh
@@ -8,6 +8,15 @@ CONFIG_CRYPTO_GHASH=y
 CONFIG_CRYPTO_POLY1305_X86_64=y
 ' >> ./target/linux/x86/64/config-5.10
 
+echo '
+CONFIG_MMC_TEST=y
+CONFIG_MMC_DEBUG=y
+CONFIG_MMC_SDHCI_ACPI=y
+CONFIG_MMC_TIFM_SD=y
+CONFIG_TIFM_CORE=y
+CONFIG_TIFM_7XX1=y
+' >> ./target/linux/x86/generic/config-5.15
+
 source ./01_customize_packages.sh
 
 exit 0

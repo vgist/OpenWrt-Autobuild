ea8b44146111aff60e956f83d83f002334fdd7a5
iqbal fasya <79214502+scoout@users.noreply.github.com>
2022-11-11 08:33:28 +0700

    Update 01_customize_packages.sh

--- scripts/01_customize_packages.sh
+++ scripts/01_customize_packages.sh
@@ -3,16 +3,6 @@
 # create directory
 [[ ! -d package/new ]] && mkdir -p package/new
 
-# Access Control
-cp -rf ../immortalwrt-luci/applications/luci-app-accesscontrol package/new/
-
-# ADBYBY Plus +
-svn export -q https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-adbyby-plus package/new/luci-app-adbyby-plus
-cp -rf ../immortalwrt-packages/net/adbyby package/new/
-
-# arpbind
-cp -rf ../immortalwrt-luci/applications/luci-app-arpbind package/new/
-
 # AutoCore
 cp -rf ../immortalwrt/package/emortal/autocore package/new/
 cp -rf ../immortalwrt/package/utils/mhz package/utils/
@@ -20,18 +10,8 @@ cp -rf ../immortalwrt-luci/modules/luci-base/root/usr/libexec/rpcd/luci feeds/lu
 # grant getCPUUsage access
 sed -i 's|"getTempInfo"|"getTempInfo", "getCPUBench", "getCPUUsage"|g' package/new/autocore/files/generic/luci-mod-status-autocore.json
 
-# automount
-cp -rf ../lede/package/lean/automount package/new/
-cp -rf ../lede/package/lean/ntfs3-mount package/new/
-# backport ntfs3 patches
-patch -p1 -i ../patches/kernel-add-kmod-fs-ntfs3.patch
-cp -rf ../lede/target/linux/generic/files-5.10 target/linux/generic/
-
 # cpufreq
 cp -rf ../immortalwrt-luci/applications/luci-app-cpufreq package/new/
-
-# DDNS
-cp -rf ../immortalwrt-packages/net/ddns-scripts_{aliyun,dnspod} package/new/
 
 # dnsmasq: add filter aaa option
 cp -rf ../patches/910-add-filter-aaaa-option-support.patch package/network/services/dnsmasq/patches/
@@ -41,20 +21,14 @@ patch -d feeds/luci -p1 -i ../../../patches/filter-aaaa-luci.patch
 # dnsmasq: use nft ruleset for dns_redirect
 patch -p1 -i ../patches/dnsmasq-use-nft-ruleset-for-dns_redirect.patch
 
-# Filetransfer
-cp -rf ../immortalwrt-luci/applications/luci-app-filetransfer package/new/
-cp -rf ../immortalwrt-luci/libs/luci-lib-fs package/new/
-
 # FullCone nat for nftables
 # patch kernel
 cp -f ../immortalwrt/target/linux/generic/hack-5.10/952-net-conntrack-events-support-multiple-registrant.patch target/linux/generic/hack-5.10/
 # fullconenat-nft
 cp -rf ../immortalwrt/package/network/utils/fullconenat-nft package/network/utils/
 # patch libnftnl
-#cp -rf ../immortalwrt/package/libs/libnftnl/patches package/libs/libnftnl/
-#sed -i '/PKG_INSTALL:=1/i\PKG_FIXUP:=autoreconf' package/libs/libnftnl/Makefile
-rm -rf package/libs/libnftnl
-cp -rf ../immortalwrt/package/libs/libnftnl package/libs/
+cp -rf ../immortalwrt/package/libs/libnftnl/patches package/libs/libnftnl/
+sed -i '/PKG_INSTALL:=1/i\PKG_FIXUP:=autoreconf' package/libs/libnftnl/Makefile
 # patch nftables
 #cp -f ../immortalwrt/package/network/utils/nftables/patches/002-nftables-add-fullcone-expression-support.patch package/network/utils/nftables/patches/
 rm -rf package/network/utils/nftables/
@@ -69,9 +43,6 @@ patch -d feeds/luci -p1 -i ../../../patches/fullconenat-luci.patch
 cp -f ../immortalwrt/package/libs/mbedtls/patches/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch package/libs/mbedtls/patches/
 cp -f ../patches/201-Camellia-block-cipher.patch package/libs/mbedtls/patches/
 
-# OLED
-svn export -q https://github.com/NateLol/luci-app-oled/trunk package/new/luci-app-oled
-
 # OpenClash
 svn export -q https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/new/luci-app-openclash
 
@@ -83,33 +54,6 @@ cp -rf ../immortalwrt-luci/applications/luci-app-ramfree package/new/
 
 # Scheduled Reboot
 cp -rf ../immortalwrt-luci/applications/luci-app-autoreboot package/new/
-
-# SeverChan
-svn export -q https://github.com/tty228/luci-app-serverchan/trunk package/new/luci-app-serverchan
-
-# ShadowsocksR Plus+
-svn export -q https://github.com/fw876/helloworld/trunk package/helloworld
-svn export -q https://github.com/coolsnowwolf/packages/trunk/net/shadowsocks-libev package/helloworld/shadowsocks-libev
-rm -rf ./feeds/packages/net/{xray-core,shadowsocks-libev}
-cp -rf ../immortalwrt-packages/net/{dns2socks,ipt2socks,microsocks,pdnsd-alt,redsocks2} package/new/
-# building ssr-libev with libmbedtls
-patch -d package/helloworld -p1 -i ../../../patches/building-ssr-libev-with-libmbedtls.patch
-
-# USB Printer
-cp -rf ../immortalwrt-luci/applications/luci-app-usb-printer package/new/
-
-# vlmcsd
-cp -rf ../immortalwrt-luci/applications/luci-app-vlmcsd package/new/
-cp -rf ../immortalwrt-packages/net/vlmcsd package/new/
-
-# xlnetacc
-cp -rf ../immortalwrt-luci/applications/luci-app-xlnetacc package/new/
-
-# Zerotier
-cp -rf ../immortalwrt-luci/applications/luci-app-zerotier package/new/
-
-# default settings and translation
-cp -rf ../default-settings package/new/
 
 # max conntrack
 sed -i 's,16384,65536,g' package/kernel/linux/files/sysctl-nf-conntrack.conf

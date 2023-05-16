4aed3ccc227cc7b94d7983367b239356b870e97c
Anonymous <458892+aieu@users.noreply.github.com>
2023-05-17 06:38:40 +0800

    drop nanopi-zeropi device

--- scripts/nanopi-neo2_customization.sh
+++ scripts/nanopi-neo2_customization.sh
@@ -2,14 +2,10 @@
 
 set -ex
 
-# Thermal sensor and cpu operating points
-cp -f ../patches/011-ARM-dts-sun8i-h3-add-thermal-sensor-and-zones.patch ./target/linux/sunxi/patches-5.4/
-
 # kernel
 echo '
 CONFIG_ARM_MODULE_PLTS=y
 CONFIG_CPU_FREQ_GOV_SCHEDUTIL=y
-CONFIG_SUN8I_THERMAL=y
 ' >> ./target/linux/sunxi/config-5.4
 
 # Set dhcp proto on lan

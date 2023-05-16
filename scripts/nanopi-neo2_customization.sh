f6ad7287e1acbe3b6891f9a4d5dccf978c7a6435
Anonymous <458892+aieu@users.noreply.github.com>
2023-05-16 16:26:18 +0800

    add nanopi neo2
    (cherry picked from commit c10c0c32b02689f5a296c8c13b1fd99339c8a802)

---
+++ scripts/nanopi-neo2_customization.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+# kernel
+echo '
+CONFIG_ARM_MODULE_PLTS=y
+CONFIG_CPU_FREQ_GOV_SCHEDUTIL=y
+CONFIG_SUN8I_THERMAL=y
+' >> ./target/linux/sunxi/config-5.15
+
+# Set dhcp proto on lan
+echo '
+#!/bin/sh
+
+uci del network.lan.ipaddr
+uci del network.lan.netmask
+uci set network.lan.proto='dhcp'
+uci commit network
+' >> ./package/base-files/files/etc/uci-defaults/dhcp-proto-lan
+
+source ./01_customize_packages.sh
+
+exit 0

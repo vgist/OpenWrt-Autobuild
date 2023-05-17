d8389f9102f6fae86ffc2140fcea6af9800525e6
Anonymous <458892+aieu@users.noreply.github.com>
2023-05-17 10:07:27 +0800

    nanopi-neo2: model name patches

--- scripts/nanopi-neo2_customization.sh
+++ scripts/nanopi-neo2_customization.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+# model name patch
+cp -f ../immortalwrt/target/linux/generic/hack-5.4/312-arm64-cpuinfo-Add-model-name-in-proc-cpuinfo-for-64bit-ta.patch target/linux/generic/hack-5.4/
+
 # kernel
 echo '
 CONFIG_ARM_MODULE_PLTS=y

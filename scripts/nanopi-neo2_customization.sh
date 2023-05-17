9020ba02000e700b3d360f108f48430d2a6c1051
Anonymous <458892+aieu@users.noreply.github.com>
2023-05-17 11:14:42 +0800

    cortexa53: kernel config

--- scripts/nanopi-neo2_customization.sh
+++ scripts/nanopi-neo2_customization.sh
@@ -9,7 +9,17 @@ cp -f ../patches/312-arm64-cpuinfo-Add-model-name-in-proc-cpuinfo-for-64bit-ta.p
 echo '
 CONFIG_ARM_MODULE_PLTS=y
 CONFIG_CPU_FREQ_GOV_SCHEDUTIL=y
-' >> ./target/linux/sunxi/config-5.4
+CONFIG_ARM64_CRYPTO=y
+CONFIG_CRYPTO_AES_ARM64=y
+CONFIG_CRYPTO_AES_ARM64_CE=y
+CONFIG_CRYPTO_AES_ARM64_CE_BLK=y
+CONFIG_CRYPTO_AES_ARM64_CE_CCM=y
+CONFIG_CRYPTO_CRCT10DIF_ARM64_CE=y
+CONFIG_CRYPTO_CRYPTD=y
+CONFIG_CRYPTO_GHASH_ARM64_CE=y
+CONFIG_CRYPTO_SHA1_ARM64_CE=y
+CONFIG_CRYPTO_SIMD=y
+' >> ./target/linux/sunxi/cortexa53/config-5.4
 
 # Set dhcp proto on lan
 echo '

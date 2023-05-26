fc13e03030d3e9546639f2c7812fe2ba9ae57775
Anonymous <458892+aieu@users.noreply.github.com>
2023-05-26 22:59:25 +0800

    neo2: cpufreq

--- scripts/nanopi-neo2_customization.sh
+++ scripts/nanopi-neo2_customization.sh
@@ -5,6 +5,10 @@ set -ex
 rm -rf ./package/boot/{uboot-sunxi,arm-trusted-firmware-sunxi}
 cp -rf ../lede/package/boot/{uboot-sunxi,arm-trusted-firmware-sunxi} package/boot/
 
+# cpufreq
+cp -f ../patches/103-sun50i-h5-add-cpu-opp-refs.patch target/linux/sunxi/patches-5.10/
+cp -f ../patches/302-sun50i-h5-add-more-cpu-operating-points.patch target/linux/sunxi/patches-5.10/
+
 # model name patch for aarch64
 cp -f ../patches/312-arm64-cpuinfo-Add-model-name-in-proc-cpuinfo-for-64bit-ta.patch target/linux/generic/hack-5.10/
 
@@ -12,19 +16,8 @@ cp -f ../patches/312-arm64-cpuinfo-Add-model-name-in-proc-cpuinfo-for-64bit-ta.p
 echo '
 CONFIG_ARM_MODULE_PLTS=y
 CONFIG_CPU_FREQ_GOV_SCHEDUTIL=y
-CONFIG_SUN8I_THERMAL=y
 # CONFIG_UCLAMP_TASK is not set
-CONFIG_ARM64_CRYPTO=y
-CONFIG_CRYPTO_AES_ARM64=y
-CONFIG_CRYPTO_AES_ARM64_CE=y
-CONFIG_CRYPTO_AES_ARM64_CE_BLK=y
-CONFIG_CRYPTO_AES_ARM64_CE_CCM=y
-CONFIG_CRYPTO_CRCT10DIF_ARM64_CE=y
-CONFIG_CRYPTO_CRYPTD=y
-CONFIG_CRYPTO_GHASH_ARM64_CE=y
-CONFIG_CRYPTO_SHA1_ARM64_CE=y
-CONFIG_CRYPTO_SIMD=y
-' >> ./target/linux/sunxi/cortexa53/config-5.10
+' >> ./target/linux/sunxi/config-5.10
 
 # Set dhcp proto on lan
 echo '

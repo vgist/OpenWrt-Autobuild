7e8b8c52ccb4d90fd820adb8362f14a7f3e35fcd
Anonymous <458892+aieu@users.noreply.github.com>
2023-05-27 08:45:12 +0800

    neo2: cpufreq and thermal patches

--- scripts/nanopi-neo2_customization.sh
+++ scripts/nanopi-neo2_customization.sh
@@ -5,10 +5,19 @@ set -ex
 # model name patch for aarch64
 cp -f ../patches/312-arm64-cpuinfo-Add-model-name-in-proc-cpuinfo-for-64bit-ta.patch target/linux/generic/hack-5.4/
 
+# sun50i h5 cpufreq patches
+curl -sSL https://github.com/immortalwrt/immortalwrt/commit/632c4c9.patch | patch -p1
+# thermal patches for sun8i h3 and sun50i h5
+curl -sSL https://github.com/immortalwrt/immortalwrt/commit/7fb1b00.patch | patch -p1
+# cpufreq for neo2
+cp -f ../patches/103-sun50i-h5-add-cpu-opp-refs.patch target/linux/sunxi/patches-5.4
+
 # kernel
 echo '
 CONFIG_ARM_MODULE_PLTS=y
 CONFIG_CPU_FREQ_GOV_SCHEDUTIL=y
+' >> ./target/linux/sunxi/config-5.4
+echo '
 CONFIG_ARM64_CRYPTO=y
 CONFIG_CRYPTO_AES_ARM64=y
 CONFIG_CRYPTO_AES_ARM64_CE=y

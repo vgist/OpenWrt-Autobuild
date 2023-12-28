79efc853a561ed628e466360849c29143a11a56c
clown <458892+aieu@users.noreply.github.com>
2023-12-28 14:13:15 +0800

    refine fullconenat

---
+++ scripts/01_fullconenat.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+
+set -ex
+
+# patch kernel
+#cp -f ../lede/target/linux/generic/hack-6.1/952-add-net-conntrack-events-support-multiple-registrant.patch target/linux/generic/hack-6.1/
+# https://github.com/coolsnowwolf/lede/issues/11211
+#sed -i 's|CONFIG_WERROR=y|# CONFIG_WERROR is not set|g' target/linux/generic/config-6.1
+curl -sSL https://github.com/coolsnowwolf/lede/files/11473487/952-add-net-conntrack-events-support-multiple-registrant.patch -o target/linux/generic/hack-6.1/952-add-net-conntrack-events-support-multiple-registrant.patch
+cp -f ../lede/target/linux/generic/hack-6.1/982-add-bcm-fullconenat-support.patch target/linux/generic/hack-6.1/
+# fullconenat-nft
+cp -rf ../immortalwrt/package/network/utils/fullconenat-nft package/network/utils/
+# libnftnl
+rm -rf ./package/libs/libnftnl
+cp -rf ../immortalwrt/package/libs/libnftnl package/libs/
+# nftables
+rm -rf ./package/network/utils/nftables/
+cp -rf ../immortalwrt/package/network/utils/nftables package/network/utils/
+# firewall4
+rm -rf ./package/network/config/firewall4
+cp -rf ../immortalwrt/package/network/config/firewall4 package/network/config/
+# patch luci
+patch -d feeds/luci -p1 -i ../../../patches/fullconenat-luci.patch
